9822836c32033532cb7518bb0ac6f55e40e49070
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-02 17:43:05 +0900

    test: test_proceed_attackを追加

---
+++ coding_yusha/controller/core/test_proceed_event.py
@@ -0,0 +1,23 @@
+from coding_yusha.controller.core import proceed_event
+from coding_yusha.controller.core.event import Event
+from coding_yusha.controller.core.field import Field
+from coding_yusha.controller.core.unit import Unit
+
+
+def test_proceed_attack():
+    ally_01 = Unit("ally_01", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    allies = [ally_01]
+    enemy_01 = Unit("enemy_01", 10, 10, 10, 10, 10, 5, 10, 10, 10)
+    enemies = [enemy_01]
+    field = Field(allies, enemies)
+    event = Event('ally_01', 'enemy_01', 'attack')
+
+    expected_ally_01 = Unit("ally_01", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    expected_allies = [expected_ally_01]
+    expected_enemy_01 = Unit("enemy_01", 10, 5, 10, 10, 10, 5, 10, 10, 10)
+    expected_enemies = [expected_enemy_01]
+    expected_field = Field(expected_allies, expected_enemies)
+
+    result_field = proceed_event(event, field)
+
+    assert result_field._equals(expected_field)
