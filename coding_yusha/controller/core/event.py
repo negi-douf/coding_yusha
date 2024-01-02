dafa32a3bd675b2cfed4b6fc93ded1ecc6ea16b0
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-02 16:43:35 +0900

    fix: 具象クラスに変更

--- coding_yusha/controller/core/event.py
+++ coding_yusha/controller/core/event.py
@@ -1,7 +1,9 @@
-from abc import ABC
-
-
-class Event(ABC):
+class Event():
     sender: str
     target: str
     move: str
+
+    def __init__(self, sender: str, target: str, move: str):
+        self.sender = sender
+        self.target = target
+        self.move = move
