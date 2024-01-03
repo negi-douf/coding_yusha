9ceba1442e7581a88ec65046d85dc521e4b0c66a
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-03 12:10:32 +0900

    refactor: test_unitに fixtureを追加

--- coding_yusha/controller/core/test_unit.py
+++ coding_yusha/controller/core/test_unit.py
@@ -1,11 +1,17 @@
+import pytest
+
 from coding_yusha.controller.core.event import Event
 from coding_yusha.controller.core.unit import Unit
 
 
-def test_attack():
+@pytest.fixture
+def sender_unit():
     unit = Unit("sender", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    return unit
 
-    result = unit.attack("target")
+
+def test_attack(sender_unit):
+    result = sender_unit.attack("target")
     expected_event = Event("sender", "target", "attack")
 
     assert result.sender == expected_event.sender
@@ -13,10 +19,8 @@ def test_attack():
     assert result.move == expected_event.move
 
 
-def test_special_move():
-    unit = Unit("sender", 10, 10, 10, 10, 10, 10, 10, 10, 10)
-
-    result = unit.special_move("target")
+def test_special_move(sender_unit):
+    result = sender_unit.special_move("target")
     expected_event = Event("sender", "target", "special_move")
 
     assert result.sender == expected_event.sender
@@ -24,10 +28,8 @@ def test_special_move():
     assert result.move == expected_event.move
 
 
-def test_guard():
-    unit = Unit("sender", 10, 10, 10, 10, 10, 10, 10, 10, 10)
-
-    result = unit.guard()
+def test_guard(sender_unit):
+    result = sender_unit.guard()
     expected_event = Event("sender", "sender", "guard")
 
     assert result.sender == expected_event.sender
@@ -35,15 +37,13 @@ def test_guard():
     assert result.move == expected_event.move
 
 
-def test_equals():
-    unit = Unit("sender", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+def test_equals(sender_unit):
     other = Unit("sender", 10, 10, 10, 10, 10, 10, 10, 10, 10)
 
-    assert unit._equals(other)
+    assert sender_unit._equals(other)
 
 
-def test_not_equals():
-    unit = Unit("sender", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+def test_not_equals(sender_unit):
     other = Unit("other", 10, 10, 10, 10, 10, 10, 10, 10, 10)
 
-    assert not unit._equals(other)
+    assert not sender_unit._equals(other)
