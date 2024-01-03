a66755c9e74728ebfeaf16ea41f25573ccdfa156
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-03 17:12:15 +0900

    test: test_game_masterと test_attach_parameters_for_unitを追加

--- coding_yusha/controller/core/test_attach_parameters_for_unit.py
+++ coding_yusha/controller/core/test_attach_parameters_for_unit.py
@@ -1,4 +1,5 @@
 from coding_yusha.controller.core import attach_parameters_for_unit
+from coding_yusha.controller.core.unit import Unit
 
 
 def test_list_unit_files():
@@ -8,17 +9,17 @@ def test_list_unit_files():
         "allies": [
             {
                 "py": python_files[0],
-                "yml": "ally_01.yml",
+                "yml": "coding_yusha/assets/test/ally_01.yml",
             },
             {
                 "py": python_files[1],
-                "yml": "ally_02.yml",
+                "yml": "coding_yusha/assets/test/ally_02.yml",
             },
         ],
         "enemies": [
             {
                 "py": "coding_yusha/assets/test/enemy_01.py",
-                "yml": "enemy_01.yml",
+                "yml": "coding_yusha/assets/test/enemy_01.yml",
             },
         ],
     }
@@ -26,3 +27,31 @@ def test_list_unit_files():
     result_dict = attach_parameters_for_unit.list_unit_files(dirname, python_files)
 
     assert result_dict == expected_dict
+
+
+def test_create_unit_list_from_files():
+    files = {
+        "allies": [
+            {
+                "py": "coding_yusha/assets/test/ally_01.py",
+                "yml": "coding_yusha/assets/test/ally_01.yml",
+            },
+        ],
+        "enemies": [
+            {
+                "py": "coding_yusha/assets/test/enemy_01.py",
+                "yml": "coding_yusha/assets/test/enemy_01.yml",
+            },
+        ],
+    }
+    expected_ally_01 = Unit("ally_01", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    expected_enemy_01 = Unit("enemy_01", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    expected_unit_list = {
+        "allies": [expected_ally_01],
+        "enemies": [expected_enemy_01],
+    }
+
+    result_unit_list = attach_parameters_for_unit.create_unit_list_from_files(files)
+
+    assert result_unit_list["allies"][0]._equals(expected_unit_list["allies"][0])
+    assert result_unit_list["enemies"][0]._equals(expected_unit_list["enemies"][0])
---
+++ coding_yusha/controller/test_game_master.py
@@ -0,0 +1,13 @@
+from coding_yusha.controller.core.field import Field
+from coding_yusha.controller.core.unit import Unit
+from coding_yusha.controller.game_master import GameMaster
+
+
+def test_init():
+    game_master = GameMaster("test", "./ally_01.py", "./ally_02.py")
+    ally_01 = Unit("ally_01", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    ally_02 = Unit("ally_02", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    enemy_01 = Unit("enemy_01", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    field = Field([ally_01, ally_02], [enemy_01])
+
+    assert game_master.field._equals(field)
