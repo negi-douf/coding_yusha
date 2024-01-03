1fbcac41848555735cd31d46920eb7e76e22fe1c
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-03 14:30:21 +0900

    feat: create_unit_from_ymlを実装

--- coding_yusha/controller/core/test_create_unit_from_yml.py
+++ coding_yusha/controller/core/test_create_unit_from_yml.py
@@ -1,24 +1,25 @@
 import pytest
 
+from coding_yusha.controller.core.create_unit_from_yml import create_unit_from_yml
 from coding_yusha.controller.core.unit import Unit
 
 
 def test_create_unit_from_yml():
     unit = create_unit_from_yml("test", "success.yml")
-    expected_unit = Unit("unit", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+    expected_unit = Unit("success", 10, 10, 10, 10, 10, 10, 10, 10, 10)
 
     assert unit._equals(expected_unit)
 
 
 def test_create_unit_from_yml_parameter_not_enough():
-    with pytest.raises(Exception) as e:
+    with pytest.raises(KeyError) as e:
         create_unit_from_yml("test", "not_enough.yml")
 
-    assert str(e.value) == "Unitの初期化に必要なパラメータが不足しています。"
+    assert str(e.value) == "'Unitの初期化に必要なパラメータが見つかりません。'"
 
 
 def test_create_unit_from_yml_not_found():
-    with pytest.raises(Exception) as e:
+    with pytest.raises(FileNotFoundError) as e:
         create_unit_from_yml("test", "not_found.yml")
 
     assert str(e.value) == "ファイルが見つかりません。"
