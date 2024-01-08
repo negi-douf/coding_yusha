360964a429c52febd70419094a16c8b60c9c26ab
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-08 10:55:55 +0900

    fix: いったん attach_parameters_for_unitの責任範囲は棚上げする

--- coding_yusha/controller/core/attach_parameters_for_unit.py
+++ coding_yusha/controller/core/attach_parameters_for_unit.py
@@ -20,33 +20,33 @@ def load_info(dirname: str):
     return body
 
 
-def list_unit_files(dirname: str, python_files: list[str]) -> dict[str, list[dict[str, str]]]:
-    (allies, enemies) = load_info(dirname).values()
+# def list_unit_files(dirname: str, python_files: list[str]) -> dict[str, list[dict[str, str]]]:
+#     (allies, enemies) = load_info(dirname).values()
 
-    # TODO: alliesの pyファイルの存在を確認するバリデーション
-    ally_files = []
-    for ally_yml in allies:
-        for python_file in python_files:
-            filename_from_info = path.splitext(ally_yml)[0]
-            filename_from_python = path.splitext(path.basename(python_file))[0]
-            if filename_from_info == filename_from_python:
-                ally_files.append({
-                    "py": python_file,
-                    "yml": f"coding_yusha/assets/{dirname}/{ally_yml}",
-                })
+#     # TODO: alliesの pyファイルの存在を確認するバリデーション
+#     ally_files = []
+#     for ally_yml in allies:
+#         for python_file in python_files:
+#             filename_from_info = path.splitext(ally_yml)[0]
+#             filename_from_python = path.splitext(path.basename(python_file))[0]
+#             if filename_from_info == filename_from_python:
+#                 ally_files.append({
+#                     "py": python_file,
+#                     "yml": f"coding_yusha/assets/{dirname}/{ally_yml}",
+#                 })
 
-    enemy_files = []
-    for enemy_yml in enemies:
-        filename_from_info = path.splitext(enemy_yml)[0]
-        enemy_files.append({
-            "py": f"coding_yusha/assets/{dirname}/{filename_from_info}.py",
-            "yml": f"coding_yusha/assets/{dirname}/{enemy_yml}",
-        })
+#     enemy_files = []
+#     for enemy_yml in enemies:
+#         filename_from_info = path.splitext(enemy_yml)[0]
+#         enemy_files.append({
+#             "py": f"coding_yusha/assets/{dirname}/{filename_from_info}.py",
+#             "yml": f"coding_yusha/assets/{dirname}/{enemy_yml}",
+#         })
 
-    return {
-        "allies": ally_files,
-        "enemies": enemy_files,
-    }
+#     return {
+#         "allies": ally_files,
+#         "enemies": enemy_files,
+#     }
 
 
 # def create_unit_list_from_files(files: dict[str, list[dict[str, str]]]) -> dict[str, list[Unit]]:
--- coding_yusha/controller/core/test_attach_parameters_for_unit.py
+++ coding_yusha/controller/core/test_attach_parameters_for_unit.py
@@ -1,30 +1,30 @@
-from coding_yusha.controller.core import attach_parameters_for_unit
+# from coding_yusha.controller.core import attach_parameters_for_unit
 
 
-def test_list_unit_files():
-    dirname = "test"
-    python_files = ["./ally_01.py", "public/test/ally_02.py"]
-    expected_dict = {
-        "allies": [
-            {
-                "py": python_files[0],
-                "yml": "coding_yusha/assets/test/ally_01.yml",
-            },
-            {
-                "py": python_files[1],
-                "yml": "coding_yusha/assets/test/ally_02.yml",
-            },
-        ],
-        "enemies": [
-            {
-                "py": "coding_yusha/assets/test/enemy_01.py",
-                "yml": "coding_yusha/assets/test/enemy_01.yml",
-            },
-        ],
-    }
+# def test_list_unit_files():
+#     dirname = "test"
+#     python_files = ["./ally_01.py", "public/test/ally_02.py"]
+#     expected_dict = {
+#         "allies": [
+#             {
+#                 "py": python_files[0],
+#                 "yml": "coding_yusha/assets/test/ally_01.yml",
+#             },
+#             {
+#                 "py": python_files[1],
+#                 "yml": "coding_yusha/assets/test/ally_02.yml",
+#             },
+#         ],
+#         "enemies": [
+#             {
+#                 "py": "coding_yusha/assets/test/enemy_01.py",
+#                 "yml": "coding_yusha/assets/test/enemy_01.yml",
+#             },
+#         ],
+#     }
 
-    result_dict = attach_parameters_for_unit.list_unit_files(dirname, python_files)
-    assert result_dict == expected_dict
+#     result_dict = attach_parameters_for_unit.list_unit_files(dirname, python_files)
+#     assert result_dict == expected_dict
 
 
 # def test_create_unit_list_from_files():
