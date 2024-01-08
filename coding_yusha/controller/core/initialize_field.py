99b6e1e978fc57d95d0fcad6cfedbf983085f436
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-08 14:19:42 +0900

    feat: validate_ally_py_filesを実装

--- coding_yusha/controller/core/initialize_field.py
+++ coding_yusha/controller/core/initialize_field.py
@@ -3,8 +3,9 @@ from os import path
 from yaml import safe_load
 
 
-def initialize_field(stage: str, *ally_py_files: str):
-    _ = load_stage_info(stage)
+def initialize_field(stage: str, ally_py_files: list[str]):
+    stage_info = load_stage_info(stage)
+    validate_ally_py_files(stage_info, ally_py_files)
 
 
 def load_stage_info(stage: str):
@@ -22,3 +23,22 @@ def load_stage_info(stage: str):
         raise KeyError("infoファイルの中に 'enemies'が見つかりません")
 
     return body
+
+
+def validate_ally_py_files(stage_info: dict[str, list[str]], ally_py_files: list[str]):
+    """
+        確認したいのは 2つ
+        1. 味方の pyファイルの数が info.ymlの alliesの数と一致すること
+        2. 味方の pyファイルの名前が info.ymlの alliesの名前と一致すること
+    """
+    if len(ally_py_files) != len(stage_info["allies"]):
+        raise FileNotFoundError("味方の pyファイルの数が不正です")
+
+    # 短く書くため、拡張子だけのリストを作って比較したい
+    py_filenames = [path.splitext(path.basename(ally_py))[0] for ally_py in ally_py_files]
+    yml_filenames = [path.splitext(path.basename(ally_yml))[0] for ally_yml in stage_info["allies"]]
+    for py_filename in py_filenames:
+        if py_filename not in yml_filenames:
+            # TODO: 専用のエラークラスを作る
+            raise FileNotFoundError(f"味方の pyファイルの名前が不正です: {py_filename}")
+    return True
--- coding_yusha/controller/core/test_initialize_field.py
+++ coding_yusha/controller/core/test_initialize_field.py
@@ -3,6 +3,15 @@ import pytest
 from coding_yusha.controller.core import initialize_field
 
 
+@pytest.fixture
+def test_stage_info():
+    stage_info = {
+        "allies": ["ally_01.yml", "ally_02.yml"],
+        "enemies": ["enemy_01.yml"],
+    }
+    return stage_info
+
+
 def test_load_stage_info():
     expected = {
         "allies": ["ally_01.yml", "ally_02.yml"],
@@ -33,3 +42,27 @@ def test_load_stage_info_enemies_not_found():
         _ = initialize_field.load_stage_info("test/enemies_not_found")
 
     assert str(e.value) == "\"infoファイルの中に 'enemies'が見つかりません\""
+
+
+def test_validate_ally_py_files(test_stage_info):
+    ally_py_files = ["ally_01.py", "ally_02.py"]
+
+    assert initialize_field.validate_ally_py_files(test_stage_info, ally_py_files)
+
+
+def test_validate_ally_py_files_not_enough(test_stage_info):
+    ally_py_files = ["ally_01.py"]
+
+    with pytest.raises(FileNotFoundError) as e:
+        initialize_field.validate_ally_py_files(test_stage_info, ally_py_files)
+
+    assert str(e.value) == "味方の pyファイルの数が不正です"
+
+
+def test_validate_ally_py_files_name_unmatch(test_stage_info):
+    ally_py_files = ["ally_01.py", "ally_03.py"]
+
+    with pytest.raises(FileNotFoundError) as e:
+        initialize_field.validate_ally_py_files(test_stage_info, ally_py_files)
+
+    assert str(e.value) == "味方の pyファイルの名前が不正です: ally_03"
