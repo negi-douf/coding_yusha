48c46123dff8cb8eae63159a1cf381895ca7a718
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-08 15:18:26 +0900

    fix: game_masterは棚上げして別のブランチで続きをやる

--- coding_yusha/controller/game_master.py
+++ coding_yusha/controller/game_master.py
@@ -1,5 +1,8 @@
 from coding_yusha.controller.core.field import Field
-from coding_yusha.controller.core.unit import Unit
+
+# from coding_yusha.controller.core.initialize_field import initialize_field
+
+# from coding_yusha.controller.core.unit import Unit
 
 
 class GameMaster():
@@ -7,8 +10,4 @@ class GameMaster():
 
     def __init__(self, stage: str, *ally_py_files: str):
         print(ally_py_files)
-        ally_01 = Unit("test", "ally_01.yml")
-        ally_02 = Unit("test", "ally_02.yml")
-        allies = [ally_01, ally_02]
-        enemies = [Unit("test", "enemy_01.yml")]
-        self.field = Field(allies, enemies)
+        # self.field = initialize_field(stage, ally_py_files)
