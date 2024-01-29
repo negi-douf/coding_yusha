0217fce4222759b12f623d9dcdb7b57570d43ff6
Koya Hirata <atarih.ayuok@gmail.com>
2024-01-29 15:26:16 +0900

    test: test_print_result_loseを追加

--- coding_yusha/controller/test_game_master.py
+++ coding_yusha/controller/test_game_master.py
@@ -150,3 +150,22 @@ def test_print_result_victory(capsys):
 """
 
     assert captured.out == expected
+
+
+def test_print_result_lose(capsys):
+    _game_master = GameMaster("test", "coding_yusha/assets/test/ally_01.py",
+                              "coding_yusha/assets/test/ally_02.py")
+    _game_master.field.allies[0].current_hp = 0
+    _game_master.field.allies[1].current_hp = 0
+    # 事前に入力をクリアしておきたい
+    capsys.readouterr()
+
+    _game_master.print_result()
+    captured = capsys.readouterr()
+
+    expected = """\
+敗北した...
+経過ターン数: 0
+"""
+
+    assert captured.out == expected
