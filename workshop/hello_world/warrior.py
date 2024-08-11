c6e54db20915da6ee17ba9132e2c7e2cab610c61
Koya Hirata <atarih.ayuok@gmail.com>
2024-08-11 14:59:00 +0900

    refactor: hello_world/warrior.pyのコメントを追記

--- workshop/hello_world/warrior.py
+++ workshop/hello_world/warrior.py
@@ -4,6 +4,7 @@ from coding_yusha.controller.core.unit import Unit
 class Warrior(Unit):
     def run(self):
         # implement your code here!
+        # e.g.: return self.attack("nop")
         pass
 
 
