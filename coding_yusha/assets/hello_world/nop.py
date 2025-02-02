62cefbe7461b30635e487192ed54e6df58e8628e
Koya Hirata <atarih.ayuok@gmail.com>
2025-02-02 22:32:49 +0900

    refactor: 親クラスの nop()を使うようにする

--- coding_yusha/assets/hello_world/nop.py
+++ coding_yusha/assets/hello_world/nop.py
@@ -2,11 +2,7 @@ from coding_yusha.controller.core.unit import Unit
 
 
 class Enemy01(Unit):
-    def run(self):
-        self.nop()
-
-    def nop(self):
-        print(f'{self.name}はじっとしている')
+    pass
 
 
 def main():
