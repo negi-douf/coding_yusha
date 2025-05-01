0a6f213cbe6fe0c8640a383cb99addf53efd5586
Koya Hirata <atarih.ayuok@gmail.com>
2025-05-01 23:07:01 +0900

    wip: add commentator

---
+++ coding_yusha/controller/core/test_commentator.py
@@ -0,0 +1,18 @@
+# import pytest
+
+# from coding_yusha.controller.core.commentator import Commentator
+
+# テストしたいこと
+# ユニットの行動を返せるか
+# 味方の情報を返せるか
+# 敵の一覧を返せるか
+# 生きている敵の一覧を返せるか
+
+# @pytest.fixture
+def commentator():
+    # return Commentator(allies, enemy_name_list)
+    pass
+
+
+def test_get_enemies():
+    pass
